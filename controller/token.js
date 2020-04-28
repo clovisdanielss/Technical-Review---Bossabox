a3412bb633b0ecb27b4b57449dc200949894f99b
clovisdanielss@gmail.com <clovisdanielss@gmail.com>
2020-04-28 11:33:09 -0300

    ultimas atualizações

--- controller/token.js
+++ controller/token.js
@@ -2,9 +2,18 @@ var jwt = require('jsonwebtoken')
 var express = require('express')
 var router = express.Router()
 
-router.get('/', (req, res, next) => {
-  var token = jwt.sign({ foo: 'bar', exp: Math.floor(Date.now() / 1000) + 120 }, process.env.SECRET)
-  res.json({ token: token })
+router.post('/', (req, res, next) => {
+  if (req.body.username === process.env.NAME &&
+   req.body.password === process.env.PASS) {
+    var token = jwt.sign({ foo: 'bar', exp: Math.floor(Date.now() / 1000) + 120 },
+      process.env.SECRET)
+    res.json({ token: token })
+  } else {
+    var err = new Error()
+    err.name = 'InvalidUsernameOrPassword'
+    err.status = 401
+    return next(err)
+  }
 })
 
 module.exports = router
